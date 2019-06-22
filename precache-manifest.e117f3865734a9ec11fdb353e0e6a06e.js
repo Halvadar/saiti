e13ba1ec30da7a6dd2d067d28051f9a0327a9f74
levani <levanbaidoshvili@gmail.com>
2019-06-22 05:04:02 +0400

    Updates

---
+++ precache-manifest.e117f3865734a9ec11fdb353e0e6a06e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3165e10290f8cf65d33f32fb7d1a051",
+    "url": "/saiti/index.html"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/saiti/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "243274f41a0357126446",
+    "url": "/saiti/static/css/main.0e375fe8.chunk.css"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/saiti/static/js/2.00212eb8.chunk.js"
+  },
+  {
+    "revision": "243274f41a0357126446",
+    "url": "/saiti/static/js/main.8d1a63db.chunk.js"
+  },
+  {
+    "revision": "0e1e7d8990f9be1a462e",
+    "url": "/saiti/static/js/runtime~main.bd450dd9.js"
+  },
+  {
+    "revision": "3f6fdca450e0ceedb1fe3d2111918e1b",
+    "url": "/saiti/static/media/asd.3f6fdca4.png"
+  },
+  {
+    "revision": "3eecde3161ce41cf0da0e867b7018092",
+    "url": "/saiti/static/media/untitled.3eecde31.svg"
+  }
+]);
